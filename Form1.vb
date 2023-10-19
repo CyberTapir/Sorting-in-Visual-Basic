27f35ef948137a9f9a5cc8608955abadcda745ed
cybertapir <callumtbennett@gmail.com>
2023-10-20 09:29:08 +1100

    Add Selection Sort

--- Form1.vb
+++ Form1.vb
@@ -22,8 +22,24 @@
         End While
         printArray()
     End Sub
-
-
+    Private Sub SelectionSort()
+        Dim EndUnsorted As Integer = 6
+        While EndUnsorted > 1
+            Dim i As Integer = 1
+            Dim Max As Integer = arr(i)
+            Dim PosMax As Integer = i
+            While i < EndUnsorted
+                i = i + 1
+                If arr(i) > Max Then
+                    Max = arr(i)
+                    PosMax = i
+                End If
+            End While
+            Swap(arr(PosMax), arr(EndUnsorted))
+            EndUnsorted = EndUnsorted - 1
+        End While
+        printArray()
+    End Sub
     Private Sub Swap(ByRef A As Integer, ByRef B As Integer)
         Dim Temp As Integer
         Temp = A
@@ -32,9 +48,9 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        BubbleSort()
+        'BubbleSort()
+        SelectionSort()
     End Sub
-
     Private Sub printArray()
         For i = 1 To 6
             ListBox1.Items.Add(arr(i))
